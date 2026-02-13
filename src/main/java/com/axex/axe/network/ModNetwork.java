be21bdbc29d1b388f7c4b31d7735e99af021c128
emty <workforemty@gmail.com>
2026-02-14 00:29:58 +0530

    Update ModNetwork.java

--- src/main/java/com/axex/axe/network/ModNetwork.java
+++ src/main/java/com/axex/axe/network/ModNetwork.java
@@ -1,21 +1,17 @@
 package com.axex.axe.network;
 
-import com.axex.axe.AxeMod;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.ChannelBuilder;
-import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.SimpleChannel;
 
 public class ModNetwork {
 
-    private static final String PROTOCOL_VERSION = "1";
-
     public static final SimpleChannel CHANNEL = ChannelBuilder
-        .named(new ResourceLocation(AxeMod.MODID, "main"))
-        .networkProtocolVersion(PROTOCOL_VERSION)
-        .clientAcceptedVersions(PROTOCOL_VERSION::equals)
-        .serverAcceptedVersions(PROTOCOL_VERSION::equals)
-        .simpleChannel();
+            .named(new ResourceLocation("axe_or_ex", "main"))
+            .networkProtocolVersion(1)
+            .clientAcceptedVersions(version -> true)
+            .serverAcceptedVersions(version -> true)
+            .simpleChannel();
 
     private static int packetId = 0;
 
