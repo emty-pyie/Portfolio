8a779d3eace78eb8e8e9ef0e3c0d9afe5b56d865
emty <workforemty@gmail.com>
2026-02-14 00:17:17 +0530

    Update ModNetwork.java

--- src/main/java/com/axex/axe/network/ModNetwork.java
+++ src/main/java/com/axex/axe/network/ModNetwork.java
@@ -2,25 +2,34 @@ package com.axex.axe.network;
 
 import com.axex.axe.AxeMod;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.network.ChannelBuilder;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.SimpleChannel;
 
-public final class ModNetwork {
+public class ModNetwork {
+
     private static final String PROTOCOL_VERSION = "1";
 
-    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
-            ResourceLocation.fromNamespaceAndPath(AxeMod.MOD_ID, "main"),
-            () -> PROTOCOL_VERSION,
-            PROTOCOL_VERSION::equals,
-            PROTOCOL_VERSION::equals
-    );
+    public static final SimpleChannel CHANNEL = ChannelBuilder
+            .named(new ResourceLocation(AxeMod.MODID, "main"))
+            .networkProtocolVersion(() -> PROTOCOL_VERSION)
+            .clientAcceptedVersions(PROTOCOL_VERSION::equals)
+            .serverAcceptedVersions(PROTOCOL_VERSION::equals)
+            .simpleChannel();
 
-    private static int id = 0;
+    private static int packetId = 0;
 
     public static void register() {
-        CHANNEL.registerMessage(id++, ThrowAxePacket.class, ThrowAxePacket::encode, ThrowAxePacket::decode, ThrowAxePacket::handle);
-    }
 
-    private ModNetwork() {
+        CHANNEL.messageBuilder(ThrowAxePacket.class, packetId++)
+                .encoder(ThrowAxePacket::encode)
+                .decoder(ThrowAxePacket::decode)
+                .consumerMainThread((packet, context) -> {
+                    var player = context.getSender();
+                    if (player != null) {
+                        ThrowAxePacket.handle(packet, player);
+                    }
+                })
+                .add();
     }
 }
