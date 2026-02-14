e778fbfaf44ae42dd237d96b41b04ba6c446d34f
emty <workforemty@gmail.com>
2026-02-14 13:38:19 +0530

    Update ModNetwork.java

--- src/main/java/com/axex/axe/network/ModNetwork.java
+++ src/main/java/com/axex/axe/network/ModNetwork.java
@@ -5,17 +5,15 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.ChannelBuilder;
 import net.minecraftforge.network.SimpleChannel;
 
-public final class ModNetwork {
+public class ModNetwork {
 
     private static final int PROTOCOL_VERSION = 1;
-
     public static SimpleChannel CHANNEL;
 
-    private ModNetwork() {}
-
     public static void register() {
+
         CHANNEL = ChannelBuilder
-                .named(ResourceLocation.tryBuild(AxeMod.MODID, "main"))
+                .named(ResourceLocation.parse(AxeMod.MODID + ":main"))
                 .networkProtocolVersion(PROTOCOL_VERSION)
                 .clientAcceptedVersions((status, version) -> true)
                 .serverAcceptedVersions((status, version) -> true)
